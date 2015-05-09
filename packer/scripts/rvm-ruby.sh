8eb95a36379a3e2d98c8a7af81f87030a3cb697f
NAKANO Hideo <pinarello.marvel@gmail.com>
2015-05-09 15:51:20 +0900

    try gpg mirror server when keys.gnupg.net is failed

--- packer/scripts/rvm-ruby.sh
+++ packer/scripts/rvm-ruby.sh
@@ -1,6 +1,9 @@
 # install rvm & ruby
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | sudo bash -s stable --ruby=2.2.1
+if [ $? -ne 0 ]; then
+  gpg2 --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+fi
+curl -sSL https://get.rvm.io | sudo bash -s stable --ruby=2.2.2
 
 echo "gem: --no-ri --no-rdoc" > /home/vagrant/.gemrc
 chown vagrant:vagrant /home/vagrant/.gemrc
