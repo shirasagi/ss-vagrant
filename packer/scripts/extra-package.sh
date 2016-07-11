61353c4a48af9df80962b8ac9f3012e90a76a5e9
NAKANO Hideo <pinarello.marvel@gmail.com>
2016-07-11 09:08:53 +0900

    enable to install tree

--- packer/scripts/extra-package.sh
+++ packer/scripts/extra-package.sh
@@ -1,4 +1,4 @@
 yum install --enablerepo=epel -y p7zip
-# yum install -y tree
+yum install -y tree
 # bash -c "source /etc/profile.d/rvm.sh; rvmsudo gem install debase"
 # bash -c "source /etc/profile.d/rvm.sh; rvmsudo gem install ruby-debug-ide"
