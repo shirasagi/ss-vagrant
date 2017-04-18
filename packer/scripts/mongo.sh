0b045fd19332435c1f42a75a46de3f47cb62e13b
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-04-18 15:00:03 +0900

    update mongodb to 3.4

--- packer/scripts/mongo.sh
+++ packer/scripts/mongo.sh
@@ -9,12 +9,13 @@ gpgcheck=0
 enabled=1
 _EOT_
 else
-cat << _EOT_ | tee /etc/yum.repos.d/mongodb-org-3.2.repo
-[mongodb-org-3.2]
+cat << _EOT_ | tee /etc/yum.repos.d/mongodb-org-3.4.repo
+[mongodb-org-3.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.2/x86_64/
-gpgcheck=0
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
 enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
 _EOT_
 fi
 
