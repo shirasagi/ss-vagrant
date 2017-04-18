c3693e286a6998bd144a0f40bca2b6b6a5ec57d4
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-04-18 14:52:16 +0900

    update ruby to 2.3.4

--- packer/scripts/rvm-ruby.sh
+++ packer/scripts/rvm-ruby.sh
@@ -3,7 +3,7 @@ gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB
 if [ $? -ne 0 ]; then
   gpg2 --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 fi
-curl -sSL https://get.rvm.io | sudo bash -s stable --ruby=2.3.1
+curl -sSL https://get.rvm.io | sudo bash -s stable --ruby=2.3.4
 
 echo "gem: --no-ri --no-rdoc" >> /home/vagrant/.gemrc
 echo "install: --no-document" >> /home/vagrant/.gemrc
