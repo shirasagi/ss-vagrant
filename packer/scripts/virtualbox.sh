68e2063efe249082cbb3983ec3936bc8bff05027
NAKANO Hideo <pinarello.marvel@gmail.com>
2017-05-11 17:17:06 +0900

    fix bundle install failure

--- packer/scripts/virtualbox.sh
+++ packer/scripts/virtualbox.sh
@@ -6,6 +6,9 @@ umount /mnt
 rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
 rm -rf /home/vagrant/.vbox_version
 
-# fix VirtualBox 5.1.20 Guest Addition Issue
-# see: http://qiita.com/poad1010/items/675ffe46e70135fff839
-rm /sbin/mount.vboxsf && ln -s /usr/lib/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf
+# VirtualBox 5.1.22 has been released and blow issue was fixed.
+#
+# # fix VirtualBox 5.1.20 Guest Addition Issue
+# # see: http://qiita.com/poad1010/items/675ffe46e70135fff839
+# rm -f /sbin/mount.vboxsf
+# ln -s /usr/lib/VBoxGuestAdditions/mount.vboxsf /sbin/mount.vboxsf
